8b3aa1fce32e67b8775f82f7c5bfaebb7f7940d5
jod <rootaipro@gmail.com>
2026-05-01 17:55:44 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,18 @@
-const CACHE_NAME = 'wedding-invitations-v1';
-const urlsToCache = ['/', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png', '/logo.png'];
+const CACHE_NAME = "wedding-invitations-v4";
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => caches.delete(key)))
+    )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
